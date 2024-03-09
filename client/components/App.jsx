39665886dda1f5581843a0c47c0c4bec78aff8ca
Marselena Sequoia <github.seaplanes@simplelogin.co>
2024-03-09 14:57:29 -0500

    import MainContainer and Form components to render in App

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import CreateNewPost from './CreateNewPost.jsx';
 // TODO: import components that the frontend team creates
 import { useSelector } from 'react-redux';
+import MainContainer from './MainContainer.jsx';
+import Form from './Form.jsx';
 
 const App = () => {
   const isLoggedIn = useSelector((state) => state.app.isLoggedIn);
   // return either the signin or main feed depending on whethere the user is logged in or not
   // TODO: replace these component names with whatever the frontend team names them
-  return isLoggedIn ? <MainContainer /> : <SignInComponent />;
+  return isLoggedIn ? <MainContainer /> : <Form />;
 };
 
 export default App;
