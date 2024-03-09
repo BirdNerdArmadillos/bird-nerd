87968d605e894b14b3abcae25b5f372c8730e34c
Marselena Sequoia <github.seaplanes@simplelogin.co>
2024-03-09 12:02:36 -0500

    Render either the main feed or signin based on whether or not a user is logged in

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 // import Board from './Board.jsx';
+// TODO: import components that the frontend team creates
+import { useSelector } from '@reduxjs/toolkit';
 
 const App = () => {
-  // return <Board />;
+  const isLoggedIn = useSelector((state) => state.app.isLoggedIn);
+  // return either the signin or main feed depending on whethere the user is logged in or not
+  // TODO: replace these component names with whatever the frontend team names them
+  return isLoggedIn ? <FeedComponent /> : <SignInComponent />;
 };
 
 export default App;
