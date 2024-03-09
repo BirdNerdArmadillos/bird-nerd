8642c70b3e84018905e5473b9f639ffec215d4fa
Marselena Sequoia <github.seaplanes@simplelogin.co>
2024-03-09 11:47:44 -0500

    add postContainerReducer to redux store

--- client/store.js
+++ client/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import tileReducer from './slices/tileSlice.js';
+import postContainerReducer from './slices/tileSlice.js';
 
 export default configureStore({
   reducer: {
-    // tiles: tileReducer,
+    postContainer: postContainerReducer,
   },
 });
