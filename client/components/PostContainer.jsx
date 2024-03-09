40a51622dfd7580f6dc9f2f00f43ee8077c6083f
Marselena Sequoia <github.seaplanes@simplelogin.co>
2024-03-09 11:27:49 -0500

    Create empty scaffolding for PostContainer component

---
+++ client/components/PostContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Post from './Post.jsx';
+
+const Display = () => {
+  return (
+    <section>
+      <Post />
+    </section>
+  );
+};
+
+export default Display;
