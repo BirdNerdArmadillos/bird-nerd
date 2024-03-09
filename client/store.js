62234d7ffbc17715925a7c3fb196a3edaeed09d0
Marselena Sequoia <github.seaplanes@simplelogin.co>
2024-03-09 14:56:58 -0500

    Import and provide appReducer

--- client/store.js
+++ client/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import postContainerReducer from './slices/postContainerSlice.js';
+import appReducer from './slices/appSlice.js';
 
 export default configureStore({
   reducer: {
     postContainer: postContainerReducer,
+    app: appReducer,
   },
 });
