b4dd951f9a3895769caba9c6baea1a260579728a
Marselena Sequoia <github.seaplanes@simplelogin.co>
2024-03-09 11:45:51 -0500

    Loop through posts and create array of posts components to render in the postContainer

--- client/components/PostContainer.jsx
+++ client/components/PostContainer.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import Post from './Post.jsx';
+// import { refresh } from '../slices/postContainerSlice.js';
+import { useSelector } from 'react-redux';
 
 const PostContainer = () => {
-  return (
-    <section>
-      <Post />
-    </section>
-  );
+  const posts = useSelector((state) => state.postContainer.posts);
+
+  const componentsArray = [];
+  posts.forEach((post, i) => {
+    componentsArray.push(
+      <Post
+        key={post._id}
+        user={post.user}
+        textContent={post.textContent}
+        datetime={post.datetime}
+        // TODO: add more props as necessary
+      />
+    );
+  });
+
+  return <section>{componentsArray}</section>;
 };
 
 export default PostContainer;
