0ca684a54c79205097407d5a4e302b1305cc9f30
tenn501 <158115485+tenn501@users.noreply.github.com>
2024-03-11 10:23:50 -0400

    added create-update-fetch-all methods

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
-const {User, Comment, Post} = require('./modelDB');
+const { User, Comment, Post } = require('./modelDB');
 
 // const userController = require('./controllers/userController');
+const postController = require('./postController');
 
 const app = express();
 const PORT = 3000;
@@ -34,21 +35,40 @@ app.post('/signup', (req, res) => {
     })
 })
 
+// display all posts;
+app.get('/display_all_posts', postController.displayAllPosts, (req,res) => {
+  res.status(200).json(res.locals.data);
+})
 
+
+// post request to /post to create a new post;
+app.post('/newpost', postController.createNewPost, (req, res) => {
+  return res.status(201).json(res.locals);
+});
+
+// edit the post;
+app.patch('/post', postController.editPost, (req, res) => {
+  return res.status(201).json(res.locals.data);
+});
+
+// add a comment to an existing post
+app.post('/comment', postController.addComment, (req, res) => {
+  return res.status(201).json(res.locals.comment);
+});
 
 
 
 // global error handler;
-// app.use((err, req, res, next) => {
-//   const defaultErr = {
-//     log: 'Express error handler caught unknown middleware error',
-//     status: 500,
-//     message: { err: 'An error occurred' },
-//   };
-//   const errorObj = Object.assign({}, defaultErr, err);
-//   console.log(errorObj.log);
-//   return res.status(errorObj.status).json(errorObj.message);
-// });
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
 
 
 app.listen(PORT, console.log(`Server listening on port ${PORT}`));
