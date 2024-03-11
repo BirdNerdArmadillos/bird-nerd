0fbae1db604e1ba8697f4c6ad2d7d2427d011222
Congke Zhao <congkezhao@s-MacBook-Pro.local>
2024-03-11 10:27:46 -0400

    finish Re auth

--- server/server.js
+++ server/server.js
@@ -1,54 +1,40 @@
 const express = require('express');
 const path = require('path');
-const {User, Comment, Post} = require('./modelDB');
+const { User, Comment, Post } = require('./modelDB');
 
 // const userController = require('./controllers/userController');
 
 const app = express();
 const PORT = 3000;
 
+const authRouter = require('./rotues/auth');
+
 app.use(express.json());
 app.use(express.urlencoded());
-
 
 // statically serve everything in the build folder on the route '/build'
 app.use('/build', express.static(path.join(__dirname, '../build')));
 // serve index.html on the route '/'
+
+app.use('/auth', authRouter);
 app.get('/', (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
 });
 
-
-// post req to create username and password; SIGN UP
-// get req to find username and password in db and match it; SIGN IN
-// get req to find username and send it to client;
-app.post('/signup', (req, res) => {
-  const { username, password } = req.body;
-  User.create({username, password})
-    .then((data) => {
-      res.locals = data;
-      return res.status(200).json(res.locals);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-})
-
-
-
-
+app.use('*', (req, res) => {
+  res.status(404).send('Not Found');
+});
 
 // global error handler;
-// app.use((err, req, res, next) => {
-//   const defaultErr = {
-//     log: 'Express error handler caught unknown middleware error',
-//     status: 500,
-//     message: { err: 'An error occurred' },
-//   };
-//   const errorObj = Object.assign({}, defaultErr, err);
-//   console.log(errorObj.log);
-//   return res.status(errorObj.status).json(errorObj.message);
-// });
-
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
 
 app.listen(PORT, console.log(`Server listening on port ${PORT}`));
