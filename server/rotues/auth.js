e42af62c1ef583a236db96ff0b1431c219ae8554
Marselena Sequoia <github.seaplanes@simplelogin.co>
2024-03-11 11:46:31 -0400

    Add logIn dispatch to auth.js

--- server/rotues/auth.js
+++ server/rotues/auth.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const userController = require('../controllers/userController');
 const router = express.Router();
+import { useDispatch } from 'react-redux';
+import { logIn } from '../../client/slices/appSlice.js';
 
 router.post('/signup', userController.createUser, (req, res) => {
+  const dispatch = useDispatch();
+  dispatch(logIn('testuser'));
   return res.status(200);
 });
 
 router.get('/signin', userController.verifyUser, (req, res) => {
+  const dispatch = useDispatch();
+  dispatch(logIn('testuser'));
   return res.status(200);
 });
 
